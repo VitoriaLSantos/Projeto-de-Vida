1f25485fbb345ce8e88e5778c9eb01da553fbf02
Vitória Laura Luiz dos Santos <vitoria.luiz.santos@escola.pr.gov.br>
2026-04-22 16:01:48 -0300

    22/04

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ console.log(botoes);
 
 for (let i=0; i<= botoes.length; i++){
 
-    console.log(botoes[i]);
     botoes[i].onclick=function() {
         botoes[i].classList.add("ativo");
         }

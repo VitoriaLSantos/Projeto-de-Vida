43c8f8678a49647eeff59d00480c17d7113f04be
Vitória Laura Luiz dos Santos <vitoria.luiz.santos@escola.pr.gov.br>
2026-05-13 15:53:08 -0300

    13/05

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 
-para(let i = 0 ; i < botoes.length; i++) {
+for(let i = 0 ; i < botoes.length; i++) {
     botoes[i].onclick = function () {
 
-        para(seja j = 0 ; j < botoes.length; j++) {
-            botoes[j].classList.remover("ativo");
+        for(let j = 0 ; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
     }
